3d59cfc41f22f357448c00ac36297f5de6ae99df
Mike McCready <michaeljmccready@gmail.com>
2015-06-08 08:47:59 -0700

    push to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	has_many :videos
+	attr_accessor :image_file_name
 	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
