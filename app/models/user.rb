5b72022bd7c5954de5f44205a83a92f5bd2d0df3
Mike McCready <michaeljmccready@gmail.com>
2015-06-11 21:50:43 -0700

    titles on video & user show pages update automatically

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
-	has_attached_file :image, styles: { small: "30x30", med: "100x100", large: "200x200", default_url: "asset_path( 'pawn.jpg' )"  }
+	has_attached_file :image, styles: { small: "33x33", med: "100x100", large: "200x200", default_url: "asset_path( 'pawn.jpg' )"  }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
